47f502cec68a5f6747681f8dbe6b7f1bf48b1cc2
Daniel Shu <danielshu98@gmail.com>
2020-04-26 22:24:47 -0700

    Created socket struct

---
+++ common/socket.go
@@ -0,0 +1,63 @@
+package common
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/nats-io/nats.go"
+	"fmt"
+)
+
+const (
+	LeaderToDeps = "LeaderToDeps"
+	DepsToLeader = "DepsToLeader"
+	LeaderToProposer = "LeaderToProposer"
+	ProposerToConsensus = "ProposerToConsensus"
+	ConsensusToProposer = "ConsensusToProposer"
+	ProposerToReplica = "ProposerToReplica"
+)
+
+type Socket struct {
+	conn *nats.Conn
+}
+
+func (s *Socket) Connect(address string) bool {
+	var err error
+
+	natsOptions := nats.Options{
+		Servers:        []string{address},
+		AllowReconnect: true,
+		MaxReconnect:   3,
+		ReconnectWait:  10,
+		//// TODO: these callbacks can be set in order to allow nats to perform failure handling.
+		//ClosedCB:          nil,
+		//DisconnectedErrCB: nil,
+	}
+	fmt.Println(natsOptions)
+
+	s.conn, err = natsOptions.Connect()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("error connecting to the NATS server")
+		return false
+	}
+	return true
+}
+
+func (s *Socket) Subscribe(subject string, response func(m *nats.Msg) ) {
+	_, err := s.conn.Subscribe(subject, response)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("error subscribing to the subject")
+	}
+}
+
+func (s *Socket) Publish(subject string, request []byte ) {
+	err := s.conn.Publish(subject,request)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("error publishing the nats request")
+	}
+}
+
