e24d45142d027718178bd85c8652e3338b3357b9
Daniel Shu <danielshu98@gmail.com>
2020-05-04 16:39:25 -0700

    removed commneted out code

--- bpaxos/setup.go
+++ bpaxos/setup.go
@@ -14,25 +14,6 @@ var (
 	leader_count = 0
 )
 
-// // Create a UDP Server listening on a port and return the port
-// func CreateUDPSocket() (int, net.Listener) {
-//     // Keep trying ports until either you exhaust all ports 
-//     // or one is available
-//     // If no ports are available, return 0
-//     var port int
-//     var l net.Listener
-//     var err error
-//     for port = 8000; port <= 65535; port++ {
-//         l, err = net.Listen("udp", fmt.Sprintf("127.0.0.1:%d", port))
-//         if err == nil {
-//             fmt.Println(fmt.Sprintf("Port %d selected", port) )
-//             return port, l
-//         }
-//     }
-
-//     return 0, nil
-// }
-
 func SetupBPaxos(isPrimary bool) {
 
 	if isPrimary {
