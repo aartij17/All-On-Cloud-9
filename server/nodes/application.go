3dd6e7a9c36dd2a6d066196ddc74288cb2aabf68
Abtin Bateni <abtinbateni@gmail.com>
2020-05-05 15:48:27 -0700

    setup connection to message channels in apps

--- server/nodes/application.go
+++ server/nodes/application.go
@@ -6,7 +6,6 @@ import (
 	"All-On-Cloud-9/messenger"
 	"context"
 	"encoding/json"
-
 	log "github.com/Sirupsen/logrus"
 
 	"github.com/nats-io/nats.go"
@@ -21,51 +20,10 @@ var (
 
 const (
 	NATS_MANUFACTURER_INBOX = "NATS_MANUFACTURER_INBOX"
+	NATS_CARRIER_INBOX = "NATS_CARRIER_INBOX"
+	NATS_BUYER_INBOX = "NATS_BUYER_INBOX"
+	NATS_SUPPLIER_INBOX = "NATS_SUPPLIER_INBOX"
 )
-
-type Manufacturer struct {
-	MsgChannel    chan *nats.Msg
-	ContractValid chan bool
-}
-
-type Carrier struct {
-	MsgChannel    chan *nats.Msg
-	ContractValid chan bool
-}
-
-type Buyer struct {
-	MsgChannel    chan *nats.Msg
-	ContractValid chan bool
-}
-
-type Supplier struct {
-	MsgChannel    chan *nats.Msg
-	ContractValid chan bool
-}
-
-func startBuyerApplication(ctx context.Context) {
-	buyer = &Buyer{
-		ContractValid: make(chan bool),
-		MsgChannel:    make(chan *nats.Msg),
-	}
-	// all the other app-specific business logic can come here.
-}
-
-func startCarrierApplication(ctx context.Context) {
-	carrier = &Carrier{
-		ContractValid: make(chan bool),
-		MsgChannel:    make(chan *nats.Msg),
-	}
-	// all the other app-specific business logic can come here.
-}
-
-func startSupplierApplication(ctx context.Context) {
-	supplier = &Supplier{
-		ContractValid: make(chan bool),
-		MsgChannel:    make(chan *nats.Msg),
-	}
-	// all the other app-specific business logic can come here.
-}
 
 func startInterAppNatsListener(ctx context.Context, msgChan chan *nats.Msg) {
 	var (
@@ -77,6 +35,27 @@ func startInterAppNatsListener(ctx context.Context, msgChan chan *nats.Msg) {
 			_ = json.Unmarshal(natsMsg.Data, &msg)
 
 		}
+	}
+}
+
+
+
+type Manufacturer struct {
+	MsgChannel    chan *nats.Msg
+	ContractValid chan bool
+}
+
+func (m *Manufacturer) subToInterAppNats(ctx context.Context) {
+	var (
+		err error
+	)
+	err = messenger.SubscribeToInbox(ctx, AppServer.NatsConn, NATS_MANUFACTURER_INBOX, m.MsgChannel)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":       err.Error(),
+			"application": config.APP_MANUFACTURER,
+			"topic":       NATS_MANUFACTURER_INBOX,
+		}).Error("error subscribing to the nats topic")
 	}
 }
 
@@ -98,16 +77,89 @@ func startManufacturerApplication(ctx context.Context) {
 	startInterAppNatsListener(ctx, manufacturer.MsgChannel)
 }
 
-func (m *Manufacturer) subToInterAppNats(ctx context.Context) {
+type Carrier struct {
+	MsgChannel    chan *nats.Msg
+	ContractValid chan bool
+}
+
+func (c *Carrier) subToInterAppNats(ctx context.Context) {
 	var (
 		err error
 	)
-	err = messenger.SubscribeToInbox(ctx, AppServer.NatsConn, NATS_MANUFACTURER_INBOX, m.MsgChannel)
+	err = messenger.SubscribeToInbox(ctx, AppServer.NatsConn, NATS_CARRIER_INBOX, c.MsgChannel)
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error":       err.Error(),
-			"application": config.APP_MANUFACTURER,
-			"topic":       NATS_MANUFACTURER_INBOX,
+			"application": config.APP_CARRIER,
+			"topic":       NATS_CARRIER_INBOX,
 		}).Error("error subscribing to the nats topic")
 	}
+}
+
+func startCarrierApplication(ctx context.Context) {
+	carrier = &Carrier{
+		ContractValid: make(chan bool),
+		MsgChannel:    make(chan *nats.Msg),
+	}
+	// all the other app-specific business logic can come here.
+	carrier.subToInterAppNats(ctx)
+	startInterAppNatsListener(ctx, carrier.MsgChannel)
+}
+
+type Buyer struct {
+	MsgChannel    chan *nats.Msg
+	ContractValid chan bool
+}
+
+func (b *Buyer) subToInterAppNats(ctx context.Context) {
+	var (
+		err error
+	)
+	err = messenger.SubscribeToInbox(ctx, AppServer.NatsConn, NATS_BUYER_INBOX, b.MsgChannel)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":       err.Error(),
+			"application": config.APP_BUYER,
+			"topic":       NATS_BUYER_INBOX,
+		}).Error("error subscribing to the nats topic")
+	}
+}
+
+func startBuyerApplication(ctx context.Context) {
+	buyer = &Buyer{
+		ContractValid: make(chan bool),
+		MsgChannel:    make(chan *nats.Msg),
+	}
+	// all the other app-specific business logic can come here.
+	buyer.subToInterAppNats(ctx)
+	startInterAppNatsListener(ctx, buyer.MsgChannel)
+}
+
+type Supplier struct {
+	MsgChannel    chan *nats.Msg
+	ContractValid chan bool
+}
+
+func (s *Supplier) subToInterAppNats(ctx context.Context) {
+	var (
+		err error
+	)
+	err = messenger.SubscribeToInbox(ctx, AppServer.NatsConn, NATS_SUPPLIER_INBOX, s.MsgChannel)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":       err.Error(),
+			"application": config.APP_SUPPLIER,
+			"topic":       NATS_SUPPLIER_INBOX,
+		}).Error("error subscribing to the nats topic")
+	}
+}
+
+func startSupplierApplication(ctx context.Context) {
+	supplier = &Supplier{
+		ContractValid: make(chan bool),
+		MsgChannel:    make(chan *nats.Msg),
+	}
+	// all the other app-specific business logic can come here.
+	supplier.subToInterAppNats(ctx)
+	startInterAppNatsListener(ctx, supplier.MsgChannel)
 }
