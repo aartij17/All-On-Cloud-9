b41e9306d90a71e23a15f9835234fa9129cace15
Daniel Shu <danielshu98@gmail.com>
2020-05-21 11:50:08 -0700

    Removed unsued go tests

---
+++ bpaxos/tests/scalability_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Base case:
+# 1 leader, 50 proposers, 50 consensus nodes, 50 replicas
+
+# Flood leader with requests
+
+# Get performance throughput
+
+
+# Scale up to:
+# 1 leader, 100 proposers, 50 consensus nodes, 50 replicas
+
+# Flood leader with same number of requests
+
+# Get performance throughput
