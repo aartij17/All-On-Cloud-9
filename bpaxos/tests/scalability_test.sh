906fd401d76d3fd3d684a675d103bcdfe7d758e9
Daniel Shu <danielshu98@gmail.com>
2020-05-21 20:26:58 -0700

    Wrote basic script and main to test scalability

--- bpaxos/tests/scalability_test.sh
+++ bpaxos/tests/scalability_test.sh
@@ -2,8 +2,16 @@
 
 # Base case:
 # 1 leader, 50 proposers, 50 consensus nodes, 50 replicas
+go run main.go -nodetype 0  & # Leader
+go run main.go -nodetype 1 -nodeId 0  &# Proposer
+go run main.go -nodetype 2 & # Consensus
+go run main.go -nodetype 2  & # Consensus
+go run main.go -nodetype 2  &# Consensus
+go run main.go -nodetype 3  &
 
+echo "DONE"
 # Flood leader with requests
+
 
 # Get performance throughput
 
@@ -11,6 +19,9 @@
 # Scale up to:
 # 1 leader, 100 proposers, 50 consensus nodes, 50 replicas
 
+
 # Flood leader with same number of requests
 
+
 # Get performance throughput
+
