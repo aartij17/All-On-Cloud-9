4269d9a2aae0703e3a990ea797fe492216fd8668
Daniel Shu <danielshu98@gmail.com>
2020-06-01 16:42:53 -0700

    fixed some things. cleaned some code

--- bpaxos/tests/scalability_test.sh
+++ bpaxos/tests/scalability_test.sh
@@ -3,10 +3,9 @@
 rm -f examine.txt
 rm -f statuslog.txt
 # Base case:
-# 1 leader, 50 proposers, 50 consensus nodes, 50 replicas
-/usr/local/go/bin/go run main.go -nodetype 0  -numberProps 50 > examine.txt & # Leader
+/usr/local/go/bin/go run main.go -nodetype 0  -numberProps 5 > examine.txt & # Leader
 
-for j in `seq 0 49`;
+for j in `seq 0 4`;
 do
     /usr/local/go/bin/go run main.go -nodetype 1 -nodeId $j & # Proposer
 done
@@ -16,26 +15,7 @@ do
    /usr/local/go/bin/go run main.go -nodetype 2 & # Consensus
    /usr/local/go/bin/go run main.go -nodetype 3  & # Replica
 done
-# go run main.go -nodetype 0  -numberProps 1 & # Leader
-# go run main.go -nodetype 1 -nodeId 0  &# Proposer
-# go run main.go -nodetype 2 & # Consensus
-# go run main.go -nodetype 2  & # Consensus
-# go run main.go -nodetype 2  &# Consensus
-# go run main.go -nodetype 3  &
+nsus
 
 echo "I HATE THIS"
-# Flood leader with requests
-
-
-# Get performance throughput
-
-
-# Scale up to:
-# 1 leader, 100 proposers, 50 consensus nodes, 50 replicas
-
-
-# Flood leader with same number of requests
-
-
-# Get performance throughput
 
