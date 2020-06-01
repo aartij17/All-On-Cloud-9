18927ea1d0b6465839f1940acbc979095c590381
Ubuntu <ubuntu@euca-10-1-4-174.eucalyptus.internal>
2020-06-01 05:08:09 +0000

    changed some test parameters

--- bpaxos/tests/scalability_test.sh
+++ bpaxos/tests/scalability_test.sh
@@ -11,31 +11,12 @@ do
     /usr/local/go/bin/go run main.go -nodetype 1 -nodeId $j & # Proposer
 done
 
-for i in `seq 0 49`;
+for i in `seq 0 4`;
 do
    /usr/local/go/bin/go run main.go -nodetype 2 & # Consensus
    /usr/local/go/bin/go run main.go -nodetype 3  & # Replica
 done
-# go run main.go -nodetype 0  -numberProps 1 & # Leader
-# go run main.go -nodetype 1 -nodeId 0  &# Proposer
-# go run main.go -nodetype 2 & # Consensus
-# go run main.go -nodetype 2  & # Consensus
-# go run main.go -nodetype 2  &# Consensus
-# go run main.go -nodetype 3  &
 
-echo "I HATE THIS"
-# Flood leader with requests
+echo "build started"
 
-
-# Get performance throughput
-
-
-# Scale up to:
-# 1 leader, 100 proposers, 50 consensus nodes, 50 replicas
-
-
-# Flood leader with same number of requests
-
-
-# Get performance throughput
 
