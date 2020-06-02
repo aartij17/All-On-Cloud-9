a5257eed1ad71c973773a30820b6f5cf45dd68ea
Daniel Shu <danielshu98@gmail.com>
2020-06-01 23:05:26 -0700

    changed test parameters

--- bpaxos/tests/scalability_test.sh
+++ bpaxos/tests/scalability_test.sh
@@ -2,11 +2,10 @@
 
 rm -f examine.txt
 rm -f statuslog.txt
-# Base case:
-# 1 leader, 50 proposers, 50 consensus nodes, 50 replicas
-/usr/local/go/bin/go run main.go -nodetype 0  -numberProps 50 > examine.txt & # Leader
 
-for j in `seq 0 49`;
+/usr/local/go/bin/go run main.go -nodetype 0  -numberProps 5 > examine.txt & # Leader
+
+for j in `seq 0 4`;
 do
     /usr/local/go/bin/go run main.go -nodetype 1 -nodeId $j & # Proposer
 done
